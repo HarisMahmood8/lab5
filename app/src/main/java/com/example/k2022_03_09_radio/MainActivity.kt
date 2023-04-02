5982a19a71da3b5ebab011120758b771b5d1fc8e
Haris <Haris.mahmood@uconn.edu>
2023-04-01 22:36:53 -0400

    initial commit

--- app/src/main/java/com/example/k2022_03_09_radio/MainActivity.kt
+++ app/src/main/java/com/example/k2022_03_09_radio/MainActivity.kt
@@ -27,13 +27,13 @@ class MainActivity : AppCompatActivity() {
 
         radios.add(Radio("Radio 1: Jazz24", "http://icecast.omroep.nl/radio2-bb-mp3"))
         radios.add(Radio("Radio 2: WNYC-FM", "http://fm939.wnyc.org/wnycfm"))
-        radios.add(Radio("Radio 3: KEXP-FM", "https://live-aacplus-64.kexp.org/kexp64.aac"))
-        radios.add(Radio("Radio 4:BBC RADIO 1", "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p"))
-        radios.add(Radio("Radio 5: Absolute Radio 80s", "http://icy-e-bab-06-cr.sharp-stream.com/absoluteradio80s.mp3"))
-        radios.add(Radio("Radio 6: KUTX", "https://kutx.org/wp-content/uploads/2018/08/KUTX-989-2.mp3"))
+        radios.add(Radio("Radio 3: HEART FM", "http://media-the.musicradio.com/HeartLondonMP3"))
+        radios.add(Radio("Radio 4: Smooth Radio", "http://media-the.musicradio.com/SmoothUKMP3"))
+        radios.add(Radio("Radio 5: TalkSPORT", "https://radio.talksport.com/stream?aw_0_1st.collectionid=4527&aw_0_1st.skey=1619979784"))
+        radios.add(Radio("Radio 6: Radio Paradise", "http://stream.radioparadise.com/mp3-192"))
         radios.add(Radio("Radio 7: The Current", "https://current.stream.publicradio.org/kcmp.mp3"))
-        radios.add(Radio("Radio 8: CBC Radio One", "http://cbc_r1_tor.akacast.akamaistream.net/7/469/451661/v1/rc.akacast.akamaistream.net/cbc_r1_tor"))
-        radios.add(Radio("Radio 9: NPR News", "https://npr-ice.streamguys1.com/live.mp3"))
+        radios.add(Radio("Radio 8: Awesome 80s", "http://listen.181fm.com/181-awesome80s_128k.mp3"))
+        radios.add(Radio("Radio 9: Capital XTRA", "http://media-the.musicradio.com/CapitalXTRALondonMP3"))
         radios.add(Radio("Radio 10: KCRW", "https://kcrw.streamguys1.com/kcrw_192k_mp3_on_air"))
 
         return radios
